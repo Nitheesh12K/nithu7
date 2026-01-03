0ba8bed5b6fc8beec69530184b38713d52304644
nitheesh12k <nitheeshrock25@gmail.com>
2026-01-04 02:42:01 +0530

    Change backend port to 8080 (firewall friendly)

--- frontend/src/components/JoinForm.jsx
+++ frontend/src/components/JoinForm.jsx
@@ -25,7 +25,7 @@ export default function JoinForm() {
     setMessage('')
 
     try {
-      const response = await fetch('http://localhost:3001/api/members/join', {
+      const response = await fetch('http://localhost:8080/api/members/join', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
